bc65789f94dc22296edfde718c98b4cde1e7c816
Rohit <rohhit.rz@gmail.com>
2024-09-05 02:40:51 +0530

    commit

--- medify/src/components/Calender/TimeSlotPicker/TImeSlotPicker.jsx
+++ medify/src/components/Calender/TimeSlotPicker/TImeSlotPicker.jsx
@@ -49,22 +49,72 @@ export default function TimeSlotPicker({
           >
             Morning
           </Typography>
-          {availableSlots.morning.map((slot)=>(
+          {availableSlots.morning.map((slot) => (
             <CustomChip
-            key={slot}
-            label={slot}
-            handleClick={()=>handleClick(slot)} />
-
+              key={slot}
+              label={slot}
+              handleClick={() => handleClick(slot)}
+            />
           ))}
         </Stack>
       )}
 
-      {/* {availableSlots.afternoon.langth >0 &&( */}
-
-
-
-
+      {availableSlots.afternoon.langth > 0 && (
+        <Stack
+          direction="row"
+          alignItems="center"
+          px={{ xs: 0, md: 6 }}
+          flexWrap={"wrap"}
+        >
+          <Typography
+            width={{ xs: 1, md: "15%" }}
+            fontSize={{ xs: 14, md: 16 }}
+          >
+            Afternoon
+          </Typography>
+          {availableSlots.afternoon.map((slot) => (
+            <CustomChip
+              key={slot}
+              label={slot}
+              handleClick={() => handleClick(slot)}
+            />
+          ))}
+        </Stack>
       )}
+
+      {availableSlots.afternoon.length >0 && (
+        <Stack
+        direction="row"
+        alignItems="center"
+        px={{xs:0, md:6}}
+        flexWrap={"wrap"}
+        >
+
+            <Typography
+             width={{ xs: 1, md: "15%" }}
+             fontSize={{ xs: 14, md: 16 }}>
+                Evening
+
+            </Typography>
+
+            {availableSlots.evening.map((slot)=>(
+                <CustomChip
+                key={slot}
+                label={slot}
+                handleClick={()=>handleClick(slot)}
+                
+                
+                />
+            ))}
+
+
+
+
+
+
+        </Stack>
+      )}
+
 
 
 
